7337954fbeb39a5d6b52d2289f782cca93d67d44
Daniil Glebov <dania.57.v@gmail.com>
2018-10-21 21:33:50 +0300

    Add files for classes

---
+++ src/Classes.cpp
@@ -0,0 +1,4 @@
+//
+// Created by daniil on 21.10.18.
+//
+
---
+++ src/Classes.h
@@ -0,0 +1,8 @@
+//
+// Created by daniil on 21.10.18.
+//
+
+#ifndef MINIJAVA_CLASSES_H
+#define MINIJAVA_CLASSES_H
+
+#endif //MINIJAVA_CLASSES_H
