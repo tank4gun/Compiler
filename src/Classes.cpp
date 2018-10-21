d6e7c7db839fc4ac94d166ae008def8cc5682066
Daniil Glebov <dania.57.v@gmail.com>
2018-10-21 22:06:56 +0300

    Add some classes for expressions

--- src/Classes.cpp
+++ src/Classes.cpp
@@ -2,3 +2,139 @@
 // Created by daniil on 21.10.18.
 //
 
+class PlusExp : IExp {
+    int Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[7];
+        strcpy(name, "PlusExp");
+        return name;
+    }
+
+    const IExp* e1;
+    const IExp* e2;
+};
+
+
+class MinusExp : IExp {
+    int Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[8];
+        strcpy(name, "MinusExp");
+        return name;
+    }
+
+    const IExp* e1;
+    const IExp* e2;
+};
+
+
+class TimesExp : IExp {
+    int Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[8];
+        strcpy(name, "TimesExp");
+        return name;
+    }
+
+    const IExp* e1;
+    const IExp* e2;
+};
+
+
+class DivideExp : IExp {
+    int Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[9];
+        strcpy(name, "DivideExp");
+        return name;
+    }
+
+    const IExp* e1;
+    const IExp* e2;
+};
+
+
+class AddExp : IExp {
+    int Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[6];
+        strcpy(name, "AddExp");
+        return name;
+    }
+
+    const IExp* e1;
+    const IExp* e2;
+};
+
+
+class LessExp : IExp {
+    bool Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[7];
+        strcpy(name, "LessExp");
+        return name;
+    }
+
+    const IExp* e1;
+    const IExp* e2;
+};
+
+
+class IndexExp : IExp {
+    IExp Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[8];
+        strcpy(name, "IndexExp");
+        return name;
+    }
+
+    const IExp* e1;
+    const IExp* e2;
+};
+
+
+class TrueExp : IExp {
+    bool Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[7];
+        strcpy(name, "TrueExp");
+        return name;
+    }
+};
+
+
+class FalseExp : IExp {
+    bool Accept( IVisitor* v) const {
+        return v->visit(this);
+    }
+
+    char* Name() {
+        char* name = new char[8];
+        strcpy(name, "FalseExp");
+        return name;
+    }
+};
--- src/Classes.h
+++ src/Classes.h
@@ -2,6 +2,14 @@
 // Created by daniil on 21.10.18.
 //
 
+class IExp {
+    virtual int Accept( IVisitor* v) const = 0;
+    virtual bool Accept( IVisitor* v) const = 0;
+    virtual char* Name() const = 0;
+};
+
+
+
 #ifndef MINIJAVA_CLASSES_H
 #define MINIJAVA_CLASSES_H
 
