703a8b60a6c1ab92af13fdd74281cb7454c81b7f
iksanov <iksanov@yahoo.com>
2018-11-19 00:04:07 +0300

    add forward declaration

--- src/ST-AST/IBase.h
+++ src/ST-AST/IBase.h
@@ -1,12 +1,17 @@
 #pragma once
 
-#include "../IVisitor.h"
+#include "IVisitor.h"
 #include "LocStruct.h"
+
+class IVisitor;
 
 class IBase {
   public:
     IBase() = default;
     explicit IBase(LocStruct location) : location(location) {}
+
+    virtual void Accept(IVisitor *v) const = 0;
+    virtual char *Name() const = 0;
 
     LocStruct location;
 };
