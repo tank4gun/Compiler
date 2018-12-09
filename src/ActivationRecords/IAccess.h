1821a3f62345f14e86e6718c2a608690f775424f
Daniil Glebov <dania.57.v@gmail.com>
2018-12-09 23:11:45 +0300

    Fix IFrame

--- src/ActivationRecords/IAccess.h
+++ src/ActivationRecords/IAccess.h
@@ -1,33 +1,35 @@
 #pragma once
-
+#include "IRTree/Label.h"
+#include "IRTree/IIRExp.h"
+#include "IRTree/IIRStm.h"
 
 class IAccess {
   public:
     virtual ~IAccess() = default;
-    const IIRExp* GetExp() const = 0;
+    virtual IIRExp* GetExp() const = 0;
 };
 
 class CInRegAccess : public IAccess {
   public:
-    explicit CInRegAccess(const IRTemp& temp) : _temp(temp) {}
+    explicit CInRegAccess(Temp& temp) : _temp(temp) {}
 
     IIRExp* GetExp() const override {
-      return new IRTempExp(_temp);
+      return new TempExp(_temp);
     }
 
   private:
-    IRTemp _temp;
+    Temp _temp;
 };
 
 class CInFrameAccess : public IAccess {
   public:
-    CInFrameAccess(const IAccess* frame_addr, int offset) : _frame_addr(frame_addr), _offset(offset) {}
+    CInFrameAccess(IAccess* frame_addr, int offset) : _frame_addr(frame_addr), _offset(offset) {}
 
     IIRExp* GetExp() const override {
       if(_offset != 0) {
-        return new IRMemoryExp(new IRBinaryExp(/*TYPE*/, _frame_addr->GetExp(), new IRConstExp(_offset)));
+        return new MemoryExp(new BinaryExp(BinaryOps::PLUSOP, _frame_addr->GetExp(), new ConstExp(_offset)));
       }
-      return new IRMemoryExp(_frame_addr->GetExp());
+      return new MemoryExp(_frame_addr->GetExp());
     }
 
   private:
