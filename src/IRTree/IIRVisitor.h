2b0e371f3701c1620325676993fbffc892d17627
iksanov <iksanov@yahoo.com>
2018-12-09 17:29:47 +0300

    fix missprint in visitor

--- src/IRTree/IIRVisitor.h
+++ src/IRTree/IIRVisitor.h
@@ -19,19 +19,19 @@ class ESeqExp;
 
 class IIRVisitor {
   public:
-    virtual void visit(const ConstExp *n)  = 0;
-    virtual void visit(const NameExp *n)  = 0;
-    virtual void visit(const TempExp *n)  = 0;
-    virtual void visit(const BinaryExp *n)  = 0;
-    virtual void visit(const MemoryExp *n)  = 0;
-    virtual void visit(const CallExp *n)  = 0;
+    virtual void visit(const ConstExp *n) = 0;
+    virtual void visit(const NameExp *n) = 0;
+    virtual void visit(const TempExp *n) = 0;
+    virtual void visit(const BinaryExp *n) = 0;
+    virtual void visit(const MemoryExp *n) = 0;
+    virtual void visit(const CallExp *n) = 0;
 
-    virtual void visit(const ESeqExp *n)  = 0;
+    virtual void visit(const ESeqExp *n) = 0;
 
-    virtual void visit(const MoveStm *n)  = 0;
-    virtual void visit(const ExpStm *n)  = 0;
-    virtual void visit(const JumpStm *n)  = 0;
-    virtual void visit(const CJumpStm *n)  = 0;
-    virtual void visit(const SeqStm *n)  = 0;
-    virtual void visit(const LabelStm *n)  = 0;
+    virtual void visit(const MoveStm *n) = 0;
+    virtual void visit(const ExpStm *n) = 0;
+    virtual void visit(const JumpStm *n) = 0;
+    virtual void visit(const CJumpStm *n) = 0;
+    virtual void visit(const SeqStm *n) = 0;
+    virtual void visit(const LabelStm *n) = 0;
 };
