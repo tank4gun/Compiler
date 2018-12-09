d3aaef51acca11442e868d91cb06b9055c83a7eb
Daniil Glebov <dania.57.v@gmail.com>
2018-12-09 17:31:04 +0300

    Update activation records classes

---
+++ src/ActivationRecords/CX86MiniJavaFrame.cpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "CX86MiniJavaFrame.h"
--- src/ActivationRecords/CX86MiniJavaFrame.h
+++ src/ActivationRecords/CX86MiniJavaFrame.h
@@ -1,15 +1,16 @@
 #pragma once
-#include "CInFrameAccess.h"
-#include "CInRegAccess.h"
 #include "IFrame.h"
 
 class CX86MiniJavaFrame : public IFrame {
   public:
     ~CX86MiniJavaFrame() {}
 
-    std::map<std::string, int> vars;
+    std::map<std::string, int> local_vars;
+    std::map<std::string, int> formal_vars;
     virtual IAccess* Formal(int index) const override;
+    virtual IAccess* FindLocalOrFormal(const Symbol* name) const;
 
+    IAccess* CreateFormal(RecordTypes type, int size);
 
   private:
     int FramePointer;
