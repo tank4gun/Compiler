94ccebea9d0491484495356d57b076ed6dafa30a
iksanov <iksanov@yahoo.com>
2018-12-10 15:46:24 +0300

    fix printer

--- src/IRTree/ISubtreeWrapper.cpp
+++ src/IRTree/ISubtreeWrapper.cpp
@@ -1,28 +1,5 @@
 #include "ISubtreeWrapper.h"
 
-//const IIRExp *CondConverter::ToExp() const {
-//    assert(false);
-//    TempExp *tempExp = new TempExp(Temp());
-//    Label labelFalse;
-//    Label labelTrue;
-//    return
-//        new ESeqExp(
-//            new SeqStm(
-//                new MoveStm(tempExp, new ConstExp(1)),
-//                new SeqStm(
-//                    ToConditional(labelTrue, labelFalse),
-//                    new SeqStm(
-//                        new LabelStm(labelFalse),
-//                        new SeqStm(
-//                            new MoveStm(tempExp, new ConstExp(0)),
-//                            new LabelStm(labelTrue)
-//                        )
-//                    )
-//                )
-//            ),
-//            tempExp
-//        );
-//}
 
 IIRStm *RelCondConverter::ToConditional(const Label labelTrue, const Label labelFalse) const {
     return new CJumpStm(type, leftOp, rightOp, labelTrue, labelFalse);
