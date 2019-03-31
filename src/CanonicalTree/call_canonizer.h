c37adc147db7046dec84e35be8a7cc0100836e22
Elena Smirnova <elena_smirnova98@mail.ru>
2019-03-31 18:32:49 +0300

    canonical tree added

---
+++ src/CanonicalTree/call_canonizer.h
@@ -0,0 +1,12 @@
+//
+// Created by elena on 31.03.19.
+//
+
+#ifndef MINIJAVA_CALL_CANONIZER_H
+#define MINIJAVA_CALL_CANONIZER_H
+
+class call_canonizer {
+
+};
+
+#endif //MINIJAVA_CALL_CANONIZER_H
